2a5923e67a2abff7986bf145668ff552b3004a90
Ryustem Shaban <rstmduran@gmail.com>
2023-01-24 00:18:33 +0100

    File openers added

--- Assignment3/temp/yourCryptoExperts.py
+++ Assignment3/temp/yourCryptoExperts.py
@@ -11,3 +11,17 @@ cnx = mysql.connector.connect(user='root',
                              password='99787836',
                              host='localhost'
 )
+
+# six readers for six files.
+file = open(os.getcwd() + "\\" + "Transactions400x.csv")
+csvreader = csv.reader(file)
+file2 = open(os.getcwd() + "\\" + "OfficesTableFixed.csv")
+csvreader2 = csv.reader(file2)
+file3 = open(os.getcwd() + "\\" + "wallets.csv")
+csvreader3 = csv.reader(file3)
+file4 = open(os.getcwd() + "\\" + "customer.csv")
+csvreader4 = csv.reader(file4)
+file5 = open(os.getcwd() + "\\" + "taxDetails.csv")
+csvreader5 = csv.reader(file5)
+file6 = open(os.getcwd() + "\\" + "EmployeeTablex.csv")
+csvreader6 = csv.reader(file6)
